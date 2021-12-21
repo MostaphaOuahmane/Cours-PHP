196a55914c92c73bb45fb34da9022a1dfdefa246
MostaphaOuahmane <87656171+MostaphaOuahmane@users.noreply.github.com>
2021-12-21 16:41:09 +0100

    fdbdfbfbtrtrtht

--- 00-exos/02_exo_Array.php
+++ 00-exos/02_exo_Array.php
@@ -1,8 +1,4 @@
-<?php
-
-use function PHPSTORM_META\type;
-
-require_once '../inc/functions.php'; // APPEL DES FONCTIONS
+<?php require_once '../inc/functions.php';  // appel du fichier de fonction 
 ?>
 <!doctype html>
 <html lang="fr">
@@ -15,120 +11,212 @@ require_once '../inc/functions.php'; // APPEL DES FONCTIONS
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 
-    <title>CoursPHP - 02_Array exo </title>
+    <title>CoursPHP - Exo 02 Array</title>
     <!-- mes styles -->
     <link rel="stylesheet" href="../css/style.css">
 
 </head>
 
 <body>
-
-    <header class="container-fluid bg-light">
-        <h1 class="display-4 text-center">CoursPHP - 02_Array exo</h1>
-
+    <header class="container-fluid">
+        <h1 class="display-4">CoursPHP - Exo 02 Array</h1>
     </header>
-    <section class="container bg-white">
-        <div class="row border border-success">
-            <div class="col-md-12">
+    <!-- fin container-fluid  -->
+
+    <div class="container bg-light">
+        <section class="row border border-success">
+            <div class="col-sm-12">
                 <?php
-                $tableau1 = array("Gabin", "Arletty", "Fernadel", "Dalio", "Pauline Carton");
-                echo "<pre>";
-                print_r($tableau1);
+
+                // Méthode 1 Déclaration d'un tableau, d'un array 
+                $tableau1 = array('Gabin', 'Arletty', 'Fernandel', 'Dalio', 'Pauline Carton');
+
+                echo $tableau1; // erreur de type "array to string conversion" car on ne peut pas afficher directement un tableau
+
+                echo "<pre>"; // <pre> permet de formater le texte (saut de ligne typo à chasse fixe)
+                print_r($tableau1); // print_r affiche le contenu d'un array avec les indices 
                 echo "</pre>";
                 echo "<pre>";
-                var_dump($tableau1);
+                var_dump($tableau1); // var_dump affiche le contenu d'un array avec les types des valeurs
                 echo "</pre>";
+
+                // Méthode 2 
                 $tableau2 = ['France', 'Espagne', 'Italie', 'Portugal'];
                 echo "<pre>";
-                print_r($tableau2);
+                var_dump($tableau2); // var_dump affiche le contenu d'un array avec les types des valeurs
                 echo "</pre>";
-                echo "<pre>";
-                var_dump($tableau2);
-                echo "</pre>";
-                echo "<hr>";
+
                 jeprint_r($tableau2);
-                echo "<hr>";
-                echo "<br>";
-                debug_dump($tableau1);
-                debug_dump($tableau2[0]);
-                $tableau2[] = 'suisse';
-                debug_dump($tableau2);
-                array_push($tableau2, 'Maroc', 'coucou', 'aller');
-                debug_dump($tableau2);
-                //Array associatif
+                debug($tableau2);
+                // pour afficher une valeur du tableau on écrit son indice entre crochets
+                echo $tableau2[0] . "<br>";
+
+                debug($tableau2[2]);
+                $tableau2[] = "Suisse"; // les crochets vides signifient que l'on ajoute une valeur à la fin du tableau
+                // $tableau2[0] = "Suisse"; // si on met un indice entre les crochets on va remplacer la valeur, le tableau aura tj la même longueur
+                debug($tableau2);
+
+                // 1 tableau associatif
+                // $couleurs = array( 'b' => 'bleu', 'bl' => 'blanc', 'r' => 'rouge', );
+                // dans un tableau associatif nous pouvons choisir le nom des indices 
                 $couleurs = array(
                     'b' => 'bleu',
                     'bl' => 'blanc',
                     'r' => 'rouge',
                 );
-                echo "<hr>";
-                debug_dump($couleurs);
-                print_r($couleurs['b']);
-                echo count($couleurs);
-                echo "<br>";
-                echo sizeof($couleurs);
-                echo "<br>";
-                echo "<p> le drapeau français est($couleurs[b]),($couleurs[bl]),($couleurs[r]) </p>";
+
+                debug($couleurs);
+                // un echo d'une des valeurs de notre tableau associatif
+                echo '<p>La première couleur du tableau associatif est le ' . $couleurs['b'] . '</p>';
+                echo "<p>La première couleur du tableau associatif est le $couleurs[b]</p>"; // si l'écho est entre guillemets doubles il n'est pus util de noter l'indice associatif (ici b) entre simple quote >>>> INDISPENSABLE avec des requêtes SQL
+
+                echo "<p> le nombre de valeurs dans le tableau est de : avec count() "  . count($couleurs) . "</p>";
+                echo "<p> le nombre de valeurs dans le tableau est de :  avec sizeof() "  . sizeof($couleurs) . " </p>";
+
+                // on va faire une boucle pour afficher les valeurs d'un tableau
+                // foreach >>> "pour chaque" >>> le moyen le plus simple et automatique de parcourir un tableau
 
                 echo "<ul>";
                 foreach ($tableau1 as $acteurs) {
-                    echo "<li>" . $acteurs . "</li>";
+                    echo "<li> $acteurs </li>";
                 }
                 echo "</ul>";
+
+                // une nouvelle foreach avec le tableau2 
+                // à chaque tour de boucle la variable $pays prend les valeurs du tableau ; le mot clef "as" est obligatoire
                 echo "<ol>";
                 foreach ($tableau2 as $pays) {
-                    echo "<li>" . $pays = "welcome to " . $pays . "</li>";
+                    echo "<li> $pays </li>";
                 }
                 echo "</ol>";
+
+                $tableau1[] = "Raimu"; // rajout d'une valeur au tableau
+
+
                 echo "<ol>";
                 foreach ($tableau1 as $indice => $acteurs) {
-                    echo "<li> indice :  $indice correspond à $acteurs </li>";
+                    echo "<li>Indice : $indice correspond à $acteurs </li>"; // quand il y a 2 variables après "as" celle de gauche parcourt les indices et celle de droite parcourt les valeurs 
+                    // echo '<li>Indice : ' .$indice. ' correspond à ' .$acteurs. ' </li>';
                 }
                 echo "</ol>";
+
                 echo "<ol>";
                 foreach ($couleurs as $indice => $teinte) {
-                    echo "<li> indice :  $indice correspond à $teinte </li>";
+                    echo "<li>Indice : $indice correspond à $teinte </li>";
                 }
                 echo "</ol>";
-                echo "<hr>";
+
+                // 1 Déclarez un tableau associatif "$contacts" aavec les indices "prenom", "nom", "email" et "tel" et vous y mettez les valeurs correspondantes à un seul contact (le votre)
+                // 2 Puis avec une boucle foreach vous affichez le valeurs dans une ul
+                // 3 Puis dans une autre boucle vous affichez les valeurs dans des p sauf le prénom qui doit être dans un h3 (aide >>> if else)
 
                 $contacts = array(
-                    'prenom' => 'mostapha',
-                    'nom' => 'ouahmane',
-                    'email' => 'ouhman.mostapha@gmail.com',
-                    'tel' => '0766397744',
+                    'prenom' => 'Victor',
+                    'nom' => 'Hugo',
+                    'email' => 'victor.hugo@france.fr ',
+                    'tel' => '06 63 74 11 35',
                 );
-                debug_dump($contacts);
+
+                debug($contacts);
+
                 echo "<ul>";
-                foreach ($contacts as $valeurs) {
-                    echo "<li>" . $valeurs . "</li>";
+                foreach ($contacts as $personne) {
+                    echo "<li> $personne </li>";
                 }
                 echo "</ul>";
 
-                foreach($contacts as $indice => $valeurs)  {
-                    if ( $indice =='prenom'){ 
-                        echo "<h3> $indice => $valeurs </h3> ";
-                    }else {
-                    echo "<p> $indice => $valeurs </p> ";
+                foreach ($contacts as $indice => $personne) {
+                    // echo "<p>$indice : $infos </p>";
+                    if ($indice == 'nom') {
+                        echo "<h3>$personne</h3>";
+                    } else {
+                        echo "<p>$personne </p>";
+                    }
                 }
-            }
+                echo "<hr>";
+                // tableaux multidimensionnels un tableau avec des "sous-tableaux"
 
-          
+                $tableau_multi = array(
+                    0 => array(
+                        'prenom' => 'Jean',
+                        'nom' => 'Dujardin',
+                        'email' => 'j.dujardin@gmail.com ',
+                        'tel' => '06 23 56 98 78',
+                    ),
+                    1 => array(
+                        'prenom' => 'Marion',
+                        'nom' => 'Cotillard',
+                        'email' => 'm.cotillard@gmail.com ',
+                        'tel' => '06 25 56 89 74',
+                    ),
+                    2 => array(
+                        'prenom' => 'John',
+                        'nom' => 'Wayne',
+                    ),
+                );
+
+                debug($tableau_multi);
+                debug($tableau_multi[2]);
+
+                debug($tableau_multi[1]['prenom']);
+                for ($i = 0; $i < count($tableau_multi); $i++) {
+                    echo "<p>" . $tableau_multi[$i]['prenom'] . "</p>";
+                }
+                echo "<p>";
+                foreach ($tableau_multi as $indice => $prenom) {
+                    echo '<strong>' . $tableau_multi[$indice]['prenom'] . '</strong> <br>';
+                }
+
+                echo "</p>";
+                echo "<hr>";
+                echo "<p>";
+                foreach ($tableau_multi as $indice => $nom) {
+                    echo '<strong>' . $tableau_multi[$indice]['nom'] . '</strong> <br>';
+                }
+                echo "</p>";
+                $tailles = array('extra small', 'xl', 'S', 'M', 'XS');
+                echo "<p>";
+                echo "<ul>";
+                foreach ($tailles as $infos) {
+                    echo '<li>' . $infos . '</li>';
+                }
+                echo "</ul>";
+                echo "<hr>";
+
+                $tailles2 = array(
+                    "xs" => "XS-extra-small",
+                    "s" => "S-small",
+                    "m" => "M-medium",
+                    "xl" => "XL-extra-large",
+                );
+                echo "<hr><pre class=\"bg-warning\">1\  BOUCHE FOREACH LES TAILLES TABLEAU2 </pre>";
+                echo "<ul>";
+                foreach ($tailles2 as $indice2 => $size2) {
+                    echo "<li> $indice2 pour $size2 </li>";
+                }
+                echo "</ul>";
+
+                echo "<select>";
+                foreach ($tailles2 as $indice2 => $size2) {
+                    echo "<option value = \"$indice2\"> $size2 </option>";
+                }
+                echo "</select>";
+
                 ?>
 
             </div>
-            <div class="col-md-6">
-
-            </div>
-        </div>
-    </section>
 
 
 
+        </section>
+    </div>
 
 
-    <!-- Optional JavaScript; choose one of the two! -->
 
+
+
+
+    <!-- fin container  -->
     <!-- Option 1: Bootstrap Bundle with Popper -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 
---
+++ 00-exos/03_exo_condition.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+     <!-- Bootstrap CSS -->
+     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
+    <title>CoursPHP - 03 condition EXO</title>
+     <!-- Google Fonts -->
+     <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Merriweather:ital,wght@0,400;0,700;1,400&family=Montserrat:ital,wght@0,400;0,500;1,400&display=swap" rel="stylesheet">
+    
+    <!-- mes styles -->
+    <link rel="stylesheet" href="../css/style.css">
+</head>
+<body>
+<div class="container-fluid ">
+<h1 class="display-2 text-center text-white bg-secondary bg-gradient">CoursPHP - 03 condition EXO</h1>
+
+
+
+<?php 
+$A = 100;
+$B = 5000;
+$C = 25; 
+if($A>$B){
+    function resulta($A,$B,$C){
+        return($A+$B+$C);
+
+    };
+     $D = resulta($A,$B,$C);
+    echo "$D";
+}else{
+    echo "note function";
+}
+echo "<hr>";
+echo ($A <>$B) ? " $A est séperieur à $B ": "$A est férieur à $B";
+
+
+
+?>
+
+
+
+
+
+
+
+
+
+</div>
+
+<!-- Option 1: Bootstrap Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+
+</body>
+</html>
