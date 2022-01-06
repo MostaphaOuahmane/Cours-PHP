d552e73cdd4da869a6fa3ca718fc750bd1bb76b2
Ouahmane Mostapha <87656171+MostaphaOuahmane@users.noreply.github.com>
2022-01-06 22:25:14 +0100

    egshdhr

---
+++ 09_sécurité/01_dialogue.php
@@ -0,0 +1,71 @@
+<?php require_once '../inc/functions.php'; // APPEL DES FONCTIONS ?>
+<!doctype html>
+<html lang="fr">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
+    <title>CoursPHP - Chapitre 09 - 01 sécurité dialogue</title>
+
+    <!-- Google Fonts -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Merriweather:ital,wght@0,400;0,700;1,400&family=Montserrat:ital,wght@0,400;0,500;1,400&display=swap" rel="stylesheet">
+
+    <!-- mes styles -->
+    <link rel="stylesheet" href="../css/style.css">
+  </head>
+  <body>  
+  <?php require_once '../inc/nav.inc.php';// NAVBAR ?>
+  <main>
+    <header class="container-fluid f-header p-2">
+      <h1 class="display-4 text-center">CoursPHP - Chapitre 09 - 01 sécurité, dialogue</h1>
+      <p class="lead">SOUS TITRE</p>
+    </header> 
+    <!-- fin container-fluid header  -->
+      <div class="container bg-white mt-2 mb-2 m-auto p-2">
+  
+        <section class="row">
+  
+          <div class="col-md-6">
+            <h2>Création d'une BDD</h2>
+            <ul>
+                <li>Nom de la BDD : dialogue</li>
+                <li>1 table nom de la table : commentaires (vérifier que la table et la BDD sont avec le moteur InnoDB)</li>
+                <li>la table contient les champs suivants : </li>
+                <li>id_commentaires : INT(5) PK AI</li>
+                <li>pseudo : VARCHAR(20)</li>
+                <li>message : TEXT</li>
+                <li>date_enregistrement : DATETIME</li>
+            </ul>
+            <h2></h2>
+            <ul>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+            </ul>
+
+
+          </div>
+          <!-- fin col -->
+  
+          <div class="col-md-6">
+            <h2>TitreNiveau2</h2>
+          </div>
+          <!-- fin col -->
+          </section>
+        <!-- fin row -->  
+      </div>
+      <!-- fin container  -->
+    </main>
+      <?php require_once '../inc/footer.inc.php';// FOOTER ?>
+      <!-- Option 1: Bootstrap Bundle with Popper -->
+      <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+  </body>
+</html>
